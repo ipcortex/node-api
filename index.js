a85229c4f3b375c635b89d9e64f611e7a7c30199
Ben Gesoff <ben@ipcortex.co.uk>
2016-03-23 17:09:15 +0000

    updated to work with latest API

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 var fs = require('fs');
 var vm = require('vm');
-var nodeUtil = require('util');
 
-var Class	= require('./lib/class.js');
-var Utils	= require('./lib/utils.js');
+GLOBAL.XMLHttpRequest = require('xmlhttprequest-cookie').XMLHttpRequest;
+GLOBAL.Promise = require('es6-promise-polyfill').Promise;
+GLOBAL.WebSocket = require('websocket').w3cwebsocket;
 
 var apiFile	= __dirname + '/lib/api.js';
 
-/**
- * This wraps api.js so it's compatible with the node environment. You should be able to use the api.js from your PABX's
- * software version.
- * @param locationHost {string}
- * @param locationProto {string}
- * @param locationPort {string}
- * @returns {IPCortex}
- */
-module.exports = function(locationHost, locationProto, locationPort) {
-	var locationProto = locationProto || 'http';
-	var location = {
-		protocol: locationProto + ':',
-		host: locationHost
-	};
-	if(locationPort) {
-		location.port = parseInt(locationPort, 10);
-	}
+module.exports = (function() {
 	if(!fs.existsSync(apiFile)) {
 		if(!fs.existsSync(__dirname + '/../../lib/api.js')) {
 			throw new Error('api.js does not exist! (Have you downloaded it using updateAPI.js?)');
@@ -32,28 +16,8 @@ module.exports = function(locationHost, locationProto, locationPort) {
 			apiFile = __dirname + '/../../lib/api.js';
 		}
 	}
-	var IPCortex = {};
-	IPCortex.Utils = Utils;
-	IPCortex.XHR = Utils.XHR;
-	// Add Node's extend function here so we don't rely on undocumented functions...
-	var extender = function(origin, add) {
-		var keys = Object.keys(add);
-		var i = keys.length;
-		while (i--) {
-			origin[keys[i]] = add[keys[i]];
-		}
-		return origin;
-	};
-	var contextVars = extender(GLOBAL, {
-		location:	location,
-		Class:		Class,
-		Utils:		Utils,
-		console:	console,
-		IPCortex:	IPCortex
-	});
-	var context = vm.createContext(contextVars);
 	var api = fs.readFileSync(apiFile, 'utf8');
-	vm.runInContext(api, context, { filename: apiFile });
-	IPCortex.PBX.Auth.setHost(locationProto + '://' + locationHost);
+	vm.runInThisContext(api, { filename: apiFile });
+	IPCortex.PBX.httpStopReuse();
 	return IPCortex;
-};
+}());
